4411d2972e5d78c5346e65495c7d002d51472ac4
Wilmer Torres <torresandelizw@gmail.com>
2026-03-31 19:29:52 -0400

    Comentado el Main

--- src/WilmerProyecto4/Main.java
+++ src/WilmerProyecto4/Main.java
@@ -1,7 +1,11 @@
-package WilmerProyecto4;
 
+package WilmerProyecto4;  // Indica el paquete principal al que pertenece esta clase
+
+//Clase principal desde donde inicia la ejecución del programa
 public class Main {
+	// Método principal que Java ejecuta primero
     public static void main(String[] args) {
+    	// Crea la ventana de login y la muestra en pantalla
         new vista.LoginVentana().setVisible(true);
     }
 }
